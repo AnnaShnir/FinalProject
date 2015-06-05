ac895f82de57a266b1b06aa15ed9ad80cd8efaf4
Anna Shnir <shnir.anna@gmail.com>
2015-06-04 23:55:41 -0400

    controllers

--- PartyPlanner/app/controllers/parties_controller.rb
+++ PartyPlanner/app/controllers/parties_controller.rb
@@ -1,2 +1,43 @@
 class PartiesController < ApplicationController
+
+	def index
+		@parties = Parties.all
+	end
+
+	def show
+		@party = Party.find(params[:id])
+	end
+
+	def edit
+		@party = Party.find(params[:id])
+	end
+
+	def new
+		@party = Party.new
+	end
+
+	def create
+		@party = Party.create(party_params)
+		if @party.save
+			redirect_to(parties_path)
+		else
+			render :new
+		end
+	end
+
+	def update
+		Party.find(params[:id]).update(party_params)
+		redirect_to(parties_path)
+	end
+
+	def destroy
+		Party.find(params[:id]).destroy
+		redirect_to(parties_path)
+	end
+
+	private
+	def party_params
+		params.require(:party).permit(:location, :description, :type_of_activity, :image_url)
+	end
+
 end
--- PartyPlanner/app/controllers/users_controller.rb
+++ PartyPlanner/app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+	before_action :authenticate
+
+private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :calendar_entry)
+	end
 end
